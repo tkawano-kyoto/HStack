57acb64a044f115046bc9f4a852c21db969dee70
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-09-09 17:30:29 +0900

    Initial Commit

---
+++ HStack/HStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HStackApp.swift
+//  HStack
+//
+//  Created by 河野保 on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct HStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
